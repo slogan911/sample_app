e2740dd910130deb55288001ba303819141e2825
slogan911 <slogan911@gmail.com>
2014-06-15 23:52:39 +0900

    1

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
